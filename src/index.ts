e8810a6feb7d7bf5a5c3891ff622ad19d858794f
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2018-07-06 22:33:40 +1200

    updated typescript to use a mailer router

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,32 @@
-import * as express from 'express'
+import * as express from 'express';
+import { Router } from 'express';
+import mailer from './Mailer';
 
 class App {
   public express
+  public swaggerUi;
 
-  constructor () {
+  constructor() {
     this.express = express()
     this.mountRoutes()
   }
- 
-  private mountRoutes (): void {
-    const router = express.Router()
+
+  private mountRoutes(): void {
+
+    const router : Router = express.Router()
+
     router.get('/', (req, res) => {
       res.json({
         message: 'Hello World!'
       })
     })
+
     this.express.use('/', router)
+    this.express.use('/birds', mailer)
+
+    this.express.listen(3000, () => {
+      console.log('connected! on part 3000');
+    })
   }
 }
 
---
+++ src/mailer.ts
@@ -0,0 +1,20 @@
+import * as express from 'express';
+import { Router } from 'express';
+
+const router = Router();
+
+
+router.get('/', function(req, res) {
+    res.json({
+        message: 'Selamat World!'
+      })
+});
+
+router.get('/:id', function(req, res) {
+    res.json({
+        message: 'Hello World!'
+      })
+});
+
+export default router;
+
