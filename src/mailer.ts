80984d9db6fa3ac676f7764a2f3c6157964b2bb0
jeremy woo <jeremy.woo@akqa.com>
2018-07-12 08:41:11 +1200

    added await / async function

--- src/mailer.ts
+++ src/mailer.ts
@@ -53,50 +53,52 @@ function validateEmail(req): boolean {
 
     if (req && req.body && req.body.sender && req.body.receiver)
         return true;
-
     return false;
 }
 
-async function send(mailObject: IMailObject): Promise<any> {
-     
-    console.log('calling send function');
-
-    let transporter = NodeMailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-             user: 'nlalbndxnvcepenz@ethereal.email', // generated ethereal user
-             pass: 'MAJAcN5T5dz8SaQC6E' // generated ethereal password
-        }
-    });
-
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: mailObject.sender, // '"Fred Foo 👻" <foo@example.com>', // sender address
-        to: mailObject.receiver, // 'bar@example.com, baz@example.com', // list of receivers
-        subject: mailObject.subject, // 'Hello ✔', // Subject line
-        text: mailObject.content // 'Hello world?', // plain text body
-        //html: '<b>Hello world?</b>' // html body
-    };
-
-    // send mail with defined transport object
-    await transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-            return false;
-        }
-        console.log('Message sent: %s', info.messageId);
-        console.log('Preview URL: %s', NodeMailer.getTestMessageUrl(info));
-        return true;
-    });
+async function send(mailObject: IMailObject): Promise<any> {     
+    let result = await sendMail(mailObject);
+    console.log('state of send operation', result);
+    console.log('done! servising request');
 }
 
-router.post('/:id', function (req, res) {
-    res.json({
-        message: 'Hello World!'
-    })
-});
+function sendMail(mailObject: IMailObject) { 
+
+    console.log('send email to smpt transport');
+    return new Promise<any>((resolve, reject) => {
+
+        let transporter = NodeMailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                 user: 'nlalbndxnvcepenz@ethereal.email', // generated ethereal user
+                 pass: 'MAJAcN5T5dz8SaQC6E' // generated ethereal password
+            }
+        });
+    
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: mailObject.sender, // '"Fred Foo 👻" <foo@example.com>', // sender address
+            to: mailObject.receiver, // 'bar@example.com, baz@example.com', // list of receivers
+            subject: mailObject.subject, // 'Hello ✔', // Subject line
+            text: mailObject.content // 'Hello world?', // plain text body
+            //html: '<b>Hello world?</b>' // html body
+        };    
+        // send mail with defined transport object
+         transporter.sendMail(mailOptions, (error, info) => {
+
+            if (error) {
+                console.log(error);
+                return resolve(false);
+            }
+
+            console.log('Message sent: %s', info.messageId);
+            console.log('Preview URL: %s', NodeMailer.getTestMessageUrl(info));
+            return resolve(true);
+        });
+    });
+}
 
 export default router;
 
